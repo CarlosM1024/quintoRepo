20eea9dc597a6240b7c6c76dde885b54a0ad306d
CarlosM1024 <carlosbarca024@gmail.com>
2025-08-26 15:22:53 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Carlos Martinez Miranda",                         # Tu nombre
+    author_email="mie.cmartinez@ito-depi.edu.mx",                 # Tu correo electrónico
+    url="https://github.com/CarlosM1024/quintoRepo",     # URL del proyecto
+)
